aaed264a621aca67db0ca9b7c8f9aef890f7153e
Paul WOISARD <paulwoisard@gmail.com>
2025-09-05 15:50:11 +0200

    Update page.tsx

--- app/analytics/[videoId]/page.tsx
+++ app/analytics/[videoId]/page.tsx
@@ -1,15 +1,8 @@
 // app/analytics/[videoId]/page.tsx
 import VideoAnalytics from "@/components/VideoAnalytics";
 
-type Params = { videoId: string };
-
-export default async function AnalyticsPage({
-  params,
-}: {
-  params: Promise<Params>;
-}) {
-  const { videoId } = await params;
-
+export default async function AnalyticsPage(props: any) {
+  const { videoId } = await props.params; // params est un Promise en Next 15 canary
   return (
     <main className="p-6 max-w-7xl mx-auto">
       <VideoAnalytics videoId={videoId} platform="youtube" />
